4b92bab3322500b18d1c7385ca5b7ec939a6348f
howcleiton <cleiton_2228@hotmail.com>
2025-06-10 22:04:08 -0300

    correção de logo

--- src/pages/WelcomePage.tsx
+++ src/pages/WelcomePage.tsx
@@ -44,7 +44,7 @@ const WelcomePage = () => {
         <img
           src="/icons/logo-howwallet.png"
           alt="How Wallet Logo"
-          className="w-38 h-38 mx-auto mb-4"
+          className="w-37 h-37 mx-auto mb-4"
         />
 
         <h1 className="text-2xl font-bold mb-3">Boas-vindas à How Wallet</h1>
